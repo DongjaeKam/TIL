81ec31095980212f9390791a96a850ceb0c3ae94
KamDongjae <rkaehdwo@gmail.com>
2022-07-12 21:16:09 +0900

    22/07/12 파이썬 과제 업로드

---
+++ 파이썬 문제풀이 파일/문제풀이 7월/3번 합 구하기.py
@@ -0,0 +1,6 @@
+n=10
+output = 0
+for i in range(1,n+1) :
+    output+=i
+    
+print(output)
---
+++ 파이썬 문제풀이 파일/문제풀이 7월/6번 최대값 구하기.py
@@ -0,0 +1,9 @@
+numbers = [0, 20, 100]
+
+M = numbers[0]
+
+for n in numbers:
+    if n > M :
+        M = n
+        
+print(M)
