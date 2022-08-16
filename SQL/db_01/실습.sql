12a4e15b6736276c493c8b60bdafb3b165ee5ff4
DongjaeKam <dongjaekam1994@gmail.com>
2022-08-16 15:58:58 +0900

    220816 자료 업로드 sql

---
+++ SQL/db_01/실습.sql
@@ -0,0 +1,13 @@
+SELECT COUNT(*) FROM healthcare;
+
+SELECT * FROM healthcare WHERE age < 10 ;
+
+SELECT COUNT(*) FROM healthcare WHERE gender = 1 ;
+
+SELECT COUNT(*) FROM healthcare WHERE smoking = 3 AND is_drinking;
+
+SELECT COUNT(*) FROM healthcare WHERE va_left >= 2.0 AND va_right >=2.0;
+
+SELECT COUNT(*) FROM healthcare WHERE va_left >= 2.0 AND va_right >=2.0;
+
+SELECT sido FROM healthcare;
