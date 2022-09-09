000713d90d6a2d54e57374fec0bce156f6cf3519
DongjaeKam <dongjaekam1994@gmail.com>
2022-09-09 17:29:18 +0900

    220909 자료 업로드

---
+++ Web/실습/실습 0908/css/A.js
@@ -0,0 +1,10 @@
+function print_item_count(){
+
+    document.write(added_item_count);
+
+}
+
+function plus(){
+    added_item_count =added_item_count+1;
+    alert(added_item_count)
+}
---
+++ Web/실습/실습 0908/css/print_item_count.js
@@ -0,0 +1,15 @@
+var added_item_count = 1;
+
+
+function print_item_count(){
+
+    document.write(added_item_count);
+
+}
+
+function print_item_count(){
+
+    document.write(added_item_count);
+
+}
+
