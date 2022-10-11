f715e80c502a7cc022f7db14e190b3ba47f91d45
DongjaeKam <dongjaekam1994@gmail.com>
2022-10-11 17:50:15 +0900

    221011

---
+++ Web/장고/221011/pjt/accounts/views.py
@@ -0,0 +1,28 @@
+
+from django.shortcuts import render,  redirect
+from django.http import HttpResponse
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+
+def index(request):
+    return render(request,'accounts/index.html')
+
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:   
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+
